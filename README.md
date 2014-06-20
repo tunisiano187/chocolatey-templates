e75a90e350fad5dd0975b56f8edbf448deaafdf9
Devon Hubner <devon@hubner.org>
2014-06-20 12:05:34 -0400

    Added some notes to readme for my own future reference.

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 chocolatey-templates
 ====================
 
-Templates for creating Chocolatey (http://www.chocolatey.org/) packages
+* Templates for creating Chocolatey (http://www.chocolatey.org/) packages
+
+## Package
+* In the directory where the nuspec is:
+```bash
+cpack
+```
+
+## Test a Package
+* In the same directory as the .nupkg file
+```bash
+cinst packageName -source %cd%
+```
+
+## Push a Package
+### From the Commandline
+```bash
+cpush packageName.nupkg
+```
+
+### From Website
+* http://chocolatey.org/
+
+
+
--- truecrypt/tools/chocolateyInstall.ps1
+++ truecrypt/tools/chocolateyInstall.ps1
@@ -1,30 +1,40 @@
-﻿try {
-  # obtain download id from web
-  [void]([Reflection.Assembly]::LoadWithPartialName("System.Web"))
-  $url = 'http://www.truecrypt.org/downloads'
-  $webReq = [Net.HttpWebRequest]::Create("$url")
-  $webReq.Method = "GET"
-  
-  $webRes = $webReq.GetResponse()
-  $sr = New-Object IO.StreamReader($webRes.GetResponseStream(), $webRes.ContentEncoding)
-  $content = $sr.ReadToEnd()
-  $sr.Close()
-  $webRes.Close()
+﻿
+$packageName = 'truecrypt' # arbitrary name for the package, used in messages
+$installerType = 'exe' #only one of these two: exe or msi
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/libra
+ry/aa368542(VS.85).aspx
+$silentArgs = '/S'
 
-  if($content -match 'name="LinkT" value="([0-9a-z]+)">')
-  {
-     $did = "$($matches[1])"
-     Echo "download id = $did"
-  }
+try {
+#  # obtain download id from web
+#  [void]([Reflection.Assembly]::LoadWithPartialName("System.Web"))
+#  # $url = 'http://www.truecrypt.org/downloads'
+#  $url = 'https://https://www.grc.com/misc/truecrypt/truecrypt.htm'
+#  $webReq = [Net.HttpWebRequest]::Create("$url")
+#  $webReq.Method = "GET"
+#  
+#  $webRes = $webReq.GetResponse()
+#  $sr = New-Object IO.StreamReader($webRes.GetResponseStream(), $webRes.ContentEncoding)
+#  $content = $sr.ReadToEnd()
+#  $sr.Close()
+#  $webRes.Close()
+#
+#  if($content -match 'name="LinkT" value="([0-9a-z]+)">')
+#  {
+#     $did = "$($matches[1])"
+#     Echo "download id = $did"
+#  }
 
-  $downUrl = "http://www.truecrypt.org/download/transient/${did}/TrueCrypt%20Setup%207.1a.exe"
+  # $downUrl = "http://www.truecrypt.org/download/transient/${did}/TrueCrypt%20Setup%207.1a.exe"
+  $downUrl = "https://www.grc.com/misc/truecrypt/TrueCrypt%20Setup%207.1a.exe"
   Echo "download url = $downUrl"
   # installer, will assert administrative rights
-  Install-ChocolateyPackage 'truecrypt' 'EXE' '/S' "$downUrl" -validExitCodes @(0)
+  #Install-ChocolateyPackage 'truecrypt' 'EXE' '/S' "$downUrl" -validExitCodes @(0)
+  Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$downUrl" -validExitCodes $validExitCodes
   
   # the following is all part of error handling
-  Write-ChocolateySuccess 'truecrypt'
+  Write-ChocolateySuccess "$packageName"
 } catch {
-  Write-ChocolateyFailure 'truecrypt' "$($_.Exception.Message)"
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
   throw 
 }
