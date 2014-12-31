b5963f61b688bcda1b2389acea3dee00a23f4587
Devon Hubner <devon@hubner.org>
2014-12-31 22:41:40 +0000

    Updated version to 1.20

--- pgadmin3/tools/chocolateyInstall.ps1
+++ pgadmin3/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@ $installerType  = 'msi' #only one of these two: exe or msi
 $silentArgs     = "/quiet"
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
-$zip_file = "pgadmin3-1.18.1.zip"
-$zip_url  = "http://ftp.postgresql.org/pub/pgadmin3/release/v1.18.1/win32/$zip_file"
+$zip_file = "pgadmin3-1.20.0.zip"
+$zip_url  = "http://ftp.postgresql.org/pub/pgadmin3/release/v1.20.0/win32/$zip_file"
 $msi_file = "pgadmin3.msi"
 
 
@@ -26,8 +26,6 @@ Write-Host "  * MSI File..........: $msi_file"
 Write-Host "  * ChocolateyInstall.: $env:ChocolateyInstall"
 Write-Host ""
 
-# FILE: helpers\functions\Get-ChocolateyUnzip.ps1
-#       $7zip = Join-Path "$env:ChocolateyInstall" 'chocolateyinstall\tools\7za.exe'
 if ("x$env:ChocolateyInstall" -eq "x") {
   Write-ChocolateyFailure "$packageName" "The ChocolateyInstall Environment variable is NOT SET!, Get-ChocolateyUnzip requires the ChocolateyInstall environment variable and this installation can NOT continue without it. Set ChocolateyInstall=C:\Chocolatey"
   }
@@ -40,9 +38,6 @@ try {
   Write-Host "  * Zip File Temp: $zip_file_temp"
   Write-Host "  * Zip URL......: $zip_url"
   Write-Host ""
-# Get-ChocolateyWebFile $packageName   $fileFullPath  $url     $url64bit 
-#                       -checksum $checksum -checksumType $checksumType 
-#                       -checksum64 $checksum64 -checksumType64 $checksumType64
   Get-ChocolateyWebFile "$packageName" $zip_file_temp $zip_url $zip_url
 } catch {
   Write-ChocolateyFailure "$packageName" "Could not download zip file to $zip_file_temp. REASON: $($_.Exception.Message)"
@@ -51,21 +46,12 @@ try {
 
 
 try {
-# Get-ChocolateyUnzip $fileFullPath $destination
   Write-Host ""
   Write-Host "Unzipping file"
   Write-Host "  * FROM: $zip_file_temp"
   Write-Host "  * TO..: $tempDir"
   Write-Host ""
   Get-ChocolateyUnzip "$zip_file_temp" "$tempDir"
-  # param(
-  # [string] $fileFullPath..: This is the full path to your zip file.
-  # [string] $destination...: This is a directory where you would like the unzipped files to end up.
-  # [string] $specificFolder: OPTIONAL - This is a specific directory within zip file to extract.
-  # [string] $packageName...: OPTIONAL - This will faciliate logging unzip activity for subsequent uninstall
-  #$scriptPath = (Split-Path -parent $MyInvocation.MyCommand.Definition)
-  #Get-ChocolateyUnzip "c:\someFile.zip" $scriptPath somedirinzip\somedirinzip
-  #Get-ChocolateyUnzip "C:\Chocolatey\pgadmin3\pgadmin3-1.18.1.zip" "C:\Chocolatey\pgadmin3.extracted"
 } catch {
   Write-ChocolateyFailure "$packageName" "Could not unzip zip file. REASON: $($_.Exception.Message)"
   throw
