962c1108d2110a3f377d3ffcbec7d47e206d6608
Devon Hubner <devon@hubner.org>
2014-12-31 15:15:31 +0000

    Added OSSEC package

---
+++ ossec/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿
+$packageName    = 'ossec'
+$installerType  = 'exe'
+$url            = 'http://www.ossec.net/files/ossec-agent-win32-2.8.exe'
+$url64          = $url
+$silentArgs     = '/S'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" "$url64"
+
