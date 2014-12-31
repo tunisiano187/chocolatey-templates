da156cfa1b8b98494a8170354e91b14490ebd29e
Devon Hubner <devon@hubner.org>
2014-12-31 22:41:21 +0000

    Changed ossec to ossec-agent

--- ossec-agent/tools/chocolateyInstall.ps1
+++ ossec-agent/tools/chocolateyInstall.ps1
@@ -7,5 +7,3 @@ $silentArgs     = '/S'
 $validExitCodes = @(0)
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
-Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" "$url64"
-
